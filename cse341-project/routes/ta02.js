becb18fe89b7382b64572195d1bd82385d5069f7
NathanielSnow <sno20001@byui.edu>
2022-01-11 22:13:32 -0600

    Team 1/11/2022

--- cse341-project/routes/ta02.js
+++ cse341-project/routes/ta02.js
@@ -1,15 +1,42 @@
 //TA02 PLACEHOLDER
 // Remember, you can make more of these placeholders yourself!
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-  res.render('pages', {
-    title: 'Team Activity 02',
-    path: '/ta02', // For pug, EJS
+const users = []; //Tribett
+
+function searchUsers(user, userArray) {
+  for (let i = 0; i < userArray.length; i++) {
+    if (userArray[i] === user) {
+      return i;
+    }
+    return -1;
+  }
+}
+
+router.get("/", (req, res, next) => {
+  res.render("pages/ta02", {
+    title: "Team Activity 02",
+    path: "/ta02", // For pug, EJS
     activeTA03: true, // For HBS
     contentCSS: true, // For HBS
+    users: users,
   });
+});
+
+router.post("/addUser", (req, res, next) => {
+  users.push({ name: req.body.addUser });
+  res.redirect("/ta02/");
+});
+
+router.post("/removeUser", (req, res, next) => {
+  const name = req.body.removeUser;
+  let userPos = searchUsers(name, users);
+  const index = users.indexOf();
+  if (index > -1) {
+    users.splice(index, userPos);
+  }
+  res.redirect("/ta02/");
 });
 
 module.exports = router;
