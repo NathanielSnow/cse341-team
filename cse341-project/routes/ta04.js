421b7b736bc38707d9b1b990c813f1bde75ded60
NathanielSnow <sno20001@byui.edu>
2022-02-04 22:46:17 -0600

    update

--- cse341-project/routes/ta04.js
+++ cse341-project/routes/ta04.js
@@ -3,12 +3,35 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
+  req.session.style;
+  req.session.counter;
   res.render('pages/ta04', {
     title: 'Team Activity 04',
     path: '/ta04', // For pug, EJS
     activeTA04: true, // For HBS
     contentCSS: true, // For HBS
+    textColor: req.session.style,
+    countNumber: req.session.counter
   });
+});
+
+router.post('/change-style', (req, res, next) => {
+  req.session.style = '#b01302';
+  res.redirect('/ta04');
+});
+
+router.post('/counter', (req, res, next) => {
+  if (req.body.increase === 'true') {
+    req.session.counter++;
+  }
+  else {
+    req.session.counter--;
+  }
+  res.redirect('/ta04');
+});
+
+router.post('/reset', (req, res, next) => {
+  req.session.destroy((err) => {    console.log(err);    res.redirect('/ta04');  });
 });
 
 module.exports = router;
